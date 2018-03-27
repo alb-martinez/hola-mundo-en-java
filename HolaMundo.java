25e48aa95d6a807651f74062d9cf8c83df53da99
Alberto Martinez Ruano <almartinez.amr@gmail.com>
2018-03-27 20:25:03 +0200

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,9 @@
+/**
+ *  Muestra por pantalla la frase "Hola Mundo!"
+ *
+ *  @author Alberto Martinez
+ */
+
 public class HolaMundo {
   public static void main(String[] args) {
     System.out.println("Hola Mundo!");
